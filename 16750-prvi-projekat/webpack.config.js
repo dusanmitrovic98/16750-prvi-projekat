34ced85001215af1afa6df41c60308750d48c0d8
dusan_mitrovic <dusanmitrovic@elfak.rs>
2020-04-28 17:22:30 +0200

    Inicijalizacija-part3
    Dodato:
    -Using source maps
    -Using watch mode
    -Using webpack-dev-server
    -Babel

--- 16750-prvi-projekat/webpack.config.js
+++ 16750-prvi-projekat/webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: "./dist"
+     },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
