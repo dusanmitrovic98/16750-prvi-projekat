e0293ca31ec9c7fe183361ed572a8823e0ee57bb
dusan_mitrovic <dusanmitrovic@elfak.rs>
2020-05-05 23:44:49 +0200

    Rad na modulima.
    Dodato:
    -ts-loader,
    -typescript,
    -custom.d.ts,
    -tconfig.json,
    -src/models/people/employee/employee.ts,
    -src/models/people/person/person.ts
    -src/models/chocolate-factory/chocolate-factory.ts
    
    Modifikovano:
    -webpack.config.js

--- RWA-16750-Prvi-projekat-ES6/src/models/chocolate-factory.js
+++
@@ -1,7 +0,0 @@
-export default class ChocolateFactory {
-    constructor(name, adress, workers = new Array()) {
-        this.name = name;
-        this.adress = adress;
-        this.workers = workers;
-    }
-}
--- RWA-16750-Prvi-projekat-ES6/src/models/worker.js
+++
@@ -1,7 +0,0 @@
-export default class Worker {
-    constructor(name, lastName, machineExperience = new Boolean) {
-        this.name = name;
-        this.lastName = lastName;
-        this.machineExperience = machineExperience;
-    }
-}
