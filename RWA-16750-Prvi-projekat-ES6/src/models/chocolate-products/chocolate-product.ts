65e4db7df11be93f5d7b3a02a64a2347f579d428
dusan_mitrovic <dusanmitrovic@elfak.rs>
2020-05-11 23:21:23 +0200

    Update funkcionalnosti.
    Updated:
    -index.ts
    -chocolate-material.ts
    -pallet-jack.ts
    -employee.ts
    -truck.ts

--- RWA-16750-Prvi-projekat-ES6/src/models/chocolate-products/chocolate-product.ts
+++ RWA-16750-Prvi-projekat-ES6/src/models/chocolate-products/chocolate-product.ts
@@ -1,14 +1,76 @@
 export enum ChocolateProductType {
-    MilkChocolate = "MilkChocolate",
-    DarkChocolate = "DarkChocolate",
-    WhiteChocolate = "WhiteChocolate",
-    RubyChocolate = "RubyChocolate"
+  MilkChocolate = "MilkChocolate",
+  DarkChocolate = "DarkChocolate",
+  WhiteChocolate = "WhiteChocolate",
+  RubyChocolate = "RubyChocolate",
 }
 
+export enum ProductPackedState {
+  Packed = "Packed",
+  NotPacked = "NotPacked",
+}
 export class ChocolateProduct {
-    chocolateProductType: ChocolateProductType;
+  chocolateProductType: ChocolateProductType;
+  packedState: ProductPackedState;
 
-    constructor(chocolateProductType: ChocolateProductType = ChocolateProductType.DarkChocolate) {
-        this.chocolateProductType = chocolateProductType;
-    }
+  constructor(chocolateProductType: ChocolateProductType = ChocolateProductType.DarkChocolate) {
+    this.chocolateProductType = chocolateProductType;
+    this.packedState = ProductPackedState.NotPacked;
+  }
+
+  setProductTypeToMilkChocolate() {
+    this.chocolateProductType = ChocolateProductType.MilkChocolate;
+  }
+
+  setProductTypeToDarkChocolate() {
+    this.chocolateProductType = ChocolateProductType.DarkChocolate;
+  }
+
+  setProductTypeToWhiteChocolate() {
+    this.chocolateProductType = ChocolateProductType.WhiteChocolate;
+  }
+
+  setProductTypeToRubyChocolate() {
+    this.chocolateProductType = ChocolateProductType.RubyChocolate;
+  }
+
+  isChocolateProductTypeMilkChocolate() {
+    return this.chocolateProductType === ChocolateProductType.MilkChocolate;
+  }
+
+  isChocolateProductTypeDarkChocolate() {
+    return this.chocolateProductType === ChocolateProductType.DarkChocolate;
+  }
+
+  isChocolateProductTypeWhiteChocolate() {
+    return this.chocolateProductType === ChocolateProductType.WhiteChocolate;
+  }
+
+  isChocolateProductTypeRubyChocolate() {
+    return this.chocolateProductType === ChocolateProductType.RubyChocolate;
+  }
+
+  setPackedStateToPacked() {
+    this.packedState = ProductPackedState.Packed;
+  }
+
+  setPackedStateToNotPacke() {
+    this.packedState = ProductPackedState.NotPacked;
+  }
+
+  isPackedStatePacked() {
+    return this.packedState === ProductPackedState.Packed;
+  }
+
+  isPackedStateNotPacked() {
+    return this.packedState === ProductPackedState.NotPacked;
+  }
+
+  packIt() {
+    this.setPackedStateToPacked();
+  }
+
+  unpackIt() {
+    this.setPackedStateToNotPacke();
+  }
 }
--- RWA-16750-Prvi-projekat-ES6/src/models/people/employee/employee.ts
+++ RWA-16750-Prvi-projekat-ES6/src/models/people/employee/employee.ts
@@ -1,40 +1,73 @@
 /**
- * 
+ *
  * produce product but remove material from list of materials
  */
 
-import { Person } from '../person/person';
-import { FactorySector } from '../../chocolate-factory/chocolate-factory';
-import { ChocolateProductList } from '../../chocolate-products/chocolate-product-list';
-import { ChocolateMaterial, ChocolateMaterialType } from '../../chocolate-materials/chocolate-material';
-import { ChocolateProduct, ChocolateProductType } from '../../chocolate-products/chocolate-product';
-import { Empolyeer } from '../employeer/employeer';
+import { Person } from "../person/person";
+import { FactorySector } from "../../chocolate-factory/chocolate-factory";
+import { ChocolateProductList } from "../../chocolate-products/chocolate-product-list";
+import { ChocolateMaterial, ChocolateMaterialType } from "../../chocolate-materials/chocolate-material";
+import { ChocolateProduct, ChocolateProductType } from "../../chocolate-products/chocolate-product";
+import { Empolyeer } from "../employeer/employeer";
+
+export enum EmployeeWorkState {
+  Working = "Working",
+  LunchPause = "",
+}
 
 export class Employee extends Person {
-    factoryEmployeer: Empolyeer;
-    factorySector: FactorySector;
-    producedChocolateGoods: ChocolateProductList;
+  factoryEmployeer: Empolyeer;
+  factorySector: FactorySector;
+  producedChocolateGoods: ChocolateProductList;
 
-    constructor(name: string, lastName: string, factoryEmployeer: Empolyeer, drivingLicence: boolean = false) {
-        super(name, lastName, drivingLicence);
-        this.factoryEmployeer = factoryEmployeer;
-        this.producedChocolateGoods = new ChocolateProductList();
-    }
+  constructor(name: string, lastName: string, factoryEmployeer: Empolyeer, drivingLicence: boolean = false) {
+    super(name, lastName, drivingLicence);
+    this.factoryEmployeer = factoryEmployeer;
+    this.producedChocolateGoods = new ChocolateProductList();
+  }
 
-    produceChocolateProduct(chocolateMaterial: ChocolateMaterial) {
-        let producedChocolateProduct = new ChocolateProduct();
-        if (chocolateMaterial.chocolateMaterialType === ChocolateMaterialType.DarkChocolateMaterial) {
-            producedChocolateProduct.chocolateProductType = ChocolateProductType.DarkChocolate;
-        } else if (chocolateMaterial.chocolateMaterialType === ChocolateMaterialType.WhiteChocolateMaterial) {
-            producedChocolateProduct.chocolateProductType = ChocolateProductType.WhiteChocolate;
-        } else if (chocolateMaterial.chocolateMaterialType === ChocolateMaterialType.MilkChocolateMaterial) {
-            producedChocolateProduct.chocolateProductType = ChocolateProductType.MilkChocolate;
-        } else if (chocolateMaterial.chocolateMaterialType === ChocolateMaterialType.RubyChocolateMaterial) {
-            producedChocolateProduct.chocolateProductType = ChocolateProductType.RubyChocolate;
-        } else {
-            producedChocolateProduct = null;
-        }
-        this.producedChocolateGoods.addProductToList(producedChocolateProduct);
-        return producedChocolateProduct;
+  produceChocolateProduct(chocolateMaterial: ChocolateMaterial) {
+    let producedChocolateProduct = new ChocolateProduct();
+    if (chocolateMaterial.isMaterialTypeDarkChocolateMaterial()) {
+      return this.produceDarkChocolateProduct();
     }
+    if (chocolateMaterial.isMaterialTypeWhiteChocolateMaterial()) {
+      return this.produceWhiteChocolateProduct();
+    }
+    if (chocolateMaterial.isMaterialTypeMilkChocolateMaterial()) {
+      return this.produceMilkChocolateProduct();
+    }
+    if (chocolateMaterial.isMaterialTypeRubyChocolateMaterial()) {
+      return this.produceRubyChocolateProduct();
+    }
+    return (producedChocolateProduct = null);
+  }
+
+  produceDarkChocolateProduct() {
+    let newChocolateProduct = new ChocolateProduct();
+    return newChocolateProduct.setProductTypeToDarkChocolate();
+  }
+
+  produceWhiteChocolateProduct() {
+    let newChocolateProduct = new ChocolateProduct();
+    return newChocolateProduct.setProductTypeToWhiteChocolate();
+  }
+
+  produceMilkChocolateProduct() {
+    let newChocolateProduct = new ChocolateProduct();
+    return newChocolateProduct.setProductTypeToMilkChocolate();
+  }
+
+  produceRubyChocolateProduct() {
+    let newChocolateProduct = new ChocolateProduct();
+    return newChocolateProduct.setProductTypeToRubyChocolate();
+  }
+
+  packChocolateProduct(chocolateProductToPack: ChocolateProduct) {
+    chocolateProductToPack.packIt();
+  }
+
+  unpackChocolateProduct(chocolateProductToUnpack: ChocolateProduct) {
+    chocolateProductToUnpack.unpackIt();
+  }
 }
